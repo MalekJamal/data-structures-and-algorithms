a236c2ff42f6673038528f07720df28779359c0a
Malek Hamdan <malekhamdan82@gmail.com>
2022-08-06 02:44:02 +0300

    fix problems with the code

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -152,9 +152,8 @@ const gruffaloCrumble = {
 const listFoods = (recipe) => {
   let result = [];
   // Solution code here...
-  const res2=[];
-  const arr=recipe.ingredients.map(item=>{
-    return item;
+  result = recipe.ingredients.map(item=>{
+   return item.slice(item.indexOf(" ",4)+1)
   })
  
   
